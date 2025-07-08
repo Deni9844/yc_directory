1895e69f5d6641e419cc7312100a0f10685c1a70
Deni Thakulla <horizon9868@gmail.com>
2025-07-08 14:49:59 +0545

    Created the search box and startup-card UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,10 +5,10 @@ const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className='px-5 py-3 bg-white shadow-sm font-sans'>
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans max-w-[1800px] m-auto'>
         <nav className='flex justify-between items-center'>
             <Link href="/">
-              <Image src="/logo.png" alt='logo' width={144} height={30}/>
+              <Image src="/logo.png" alt='logo' width={155} height={48}/>
             </Link>
 
             <div className='flex items-center gap-5 text-black'>
--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { X } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
 const SearchFormReset = () => {
@@ -9,7 +11,11 @@ const SearchFormReset = () => {
     }
 
   return (
-    <button type='reset' onClick={reset}></button>
+    <button type='reset' onClick={reset}>
+        <Link href="/" className='search-btn text-white'> 
+            <X className='size-5'/>
+        </Link>
+    </button>
   )
 }
 
