7af9e89a39011f461fd93c14ac58b10f719b0345
Deni98445 <deni.thakulla@anchorpoints.io>
2025-07-08 14:49:59 +0545

    Created the search box and startup-card UI

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Form from 'next/form'
-import SearchFormReset from './SearchFormReset'
+import SearchFormReset from '@/components/SearchFormReset'
+import {Search} from 'lucide-react';
 
-const SearchForm = () => {
-    const query = "Test"
-    
+const SearchForm = ({ query }: { query?: string}) => {
+
   return (
     <Form action="/" scroll={false} className='search-form'>
         <input 
@@ -17,6 +17,10 @@ const SearchForm = () => {
             {
                 query && <SearchFormReset/>
             }
+                <button type='submit' className='search-btn text-white'> 
+                   <Search className='size-5'/>
+                </button>
+            
         </div>
     </Form>
   )
