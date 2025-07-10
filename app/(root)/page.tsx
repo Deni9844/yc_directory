84709aa065551f1176b3374f933689449d53cf07
Deni Thakulla <horizon9868@gmail.com>
2025-07-10 13:31:21 +0545

    Created GROQ query and fetched all the startups in real time

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import SearchForm from '@/components/SearchForm';
-import StartupCard from '@/components/StartupCard';
-import { client } from '@/sanity/lib/client';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+
 export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
   const query = (await searchParams).query;
+  const params = { search: query || null }
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
   return (
     <>
@@ -36,6 +38,7 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
             )}
         </ul>
       </section>
+      <SanityLive/>
     </>
   );
 }
