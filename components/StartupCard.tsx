d6c151fe07dee103d81f8a38f8f10dd2398cc65f
Deni98445 <deni.thakulla@anchorpoints.io>
2025-07-10 13:31:21 +0545

    Created GROQ query and fetched all the startups in real time

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,9 +2,21 @@ import { formatDate } from "@/lib/utils"
 import { EyeIcon} from "lucide-react"
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
 
-const StartupCard = ({ post }: {post : StratupTypeCard}) => {
-    const { _id, _createdAt, views, author:{ _id: authorId, name }, title, category, image,description } = post;
+export type StartupTypeCard = Omit<Startup,"author"> & { author?: Author}
+    
+const StartupCard = ({ post }: {post : StartupTypeCard}) => {
+    const {
+         _id,
+         _createdAt, 
+         views, 
+         author,
+         title,
+         category, 
+         image,
+         description 
+        } = post;
   return (
     <li className="startup-card group">
         <div className="flex-between">
@@ -20,14 +32,14 @@ const StartupCard = ({ post }: {post : StratupTypeCard}) => {
 
         <div className="flex-between mt-5 gap-5">
             <div className="flex-1">
-                <Link href={`/user/${authorId}`}>
-                    <p className="text-16-medium line-clamp-1">{name}</p>
+                <Link href={`/user/${author?._id}`}>
+                    <p className="text-16-medium line-clamp-1">{author?.name}</p>
                 </Link>
                 <Link href={`/startup/${_id}`}>
                     <h3 className="text-26-semibold line-clamp-1">{title}</h3>
                 </Link>
             </div>
-            <Link href={`/user/${authorId}`}>
+            <Link href={`/user/${author?._id}`}>
                 <img src="https://placehold.co/48x48" alt="placeholder" width={48} 
                 height={48} className="rounded-full"/>
             </Link>
@@ -42,7 +54,7 @@ const StartupCard = ({ post }: {post : StratupTypeCard}) => {
         </Link>
 
         <div className="flex-between gap-3 mt-5">
-            <Link href={`/?query=${category.toLowerCase()}`}>
+            <Link href={`/?query=${category?.toLowerCase()}`}>
                <p className="text-16-medium">{category}</p>
             </Link>
 
