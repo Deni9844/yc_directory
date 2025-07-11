82cfb5ceb46fe1b5aaaf48ced5a3ce9ed3445a4f
Deni98445 <deni.thakulla@anchorpoints.io>
2025-07-11 13:52:51 +0545

    Created new startup addition page and set up the sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0ce7469bd0cf5e6175949bcbde8cd899@o4509648773971968.ingest.de.sentry.io/4509648775479376",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
