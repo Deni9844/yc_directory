eb18dc0879f200c015fc737c37974f54551de75b
Deni Thakulla <horizon9868@gmail.com>
2025-07-11 14:46:23 +0545

    Created user's all the startups listing page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,55 +1,65 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { auth,signIn,signOut } from '@/auth'
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className='px-5 py-3 bg-white shadow-sm font-work-sans max-w-[1800px] m-auto'>
-        <nav className='flex justify-between items-center'>
-            <Link href="/">
-              <Image src="/logo.png" alt='logo' width={155} height={48}/>
-            </Link>
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
 
-            <div className='flex items-center gap-5 text-black'>
-                { session && session?.user ? (
-                    <>
-                      <Link href="/startup/create">
-                        <span>Create</span>
-                      </Link>
-                    
-                      <form action={async () => {
-                        "use server";
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
+              </Link>
 
-                        await signOut({ redirectTo: '/'});
-                      }}>
-                        <button type='submit'>
-                           <span>Logout</span>  
-                        </button>
-                      </form>
-                      
-                      <Link href={`/user/${session?.user?.id}`}>
-                        <span>{session?.user?.name}</span>
-                      </Link>
+              <form
+                action={async () => {
+                  "use server";
 
-                    </>
-                ): (
-                    <form action={async () => {
-                        "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
+              </form>
 
-                        await signIn('github');
-                    }}>
-                       <button type='submit'>
-                         Login
-                       </button> 
-                    </form>
-                )
+              <Link href={`/user/${session?.id}`}>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
 
-                }
-            </div>
-        </nav>
+                await signIn("github");
+              }}
+            >
+              <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-slate-300 bg-slate-900 text-slate-50 shadow hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90 h-9 px-4 py-2 login" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-github size-5 mr-2"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,8 +1,10 @@
-import { formatDate } from "@/lib/utils"
+import { cn, formatDate } from "@/lib/utils"
 import { EyeIcon} from "lucide-react"
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
+import { auth } from "@/auth";
 
 export type StartupTypeCard = Omit<Startup,"author"> & { author?: Author}
     
@@ -40,7 +42,7 @@ const StartupCard = ({ post }: {post : StartupTypeCard}) => {
                 </Link>
             </div>
             <Link href={`/user/${author?._id}`}>
-                <img src="https://placehold.co/48x48" alt="placeholder" width={48} 
+                <img src={author?.image} alt={author?.name} width={48} 
                 height={48} className="rounded-full"/>
             </Link>
         </div>
@@ -65,5 +67,15 @@ const StartupCard = ({ post }: {post : StartupTypeCard}) => {
     </li>
   )
 }
+
+export const StartupCardSkeleton = () => (
+  <>
+    {[0, 1, 2, 3, 4].map((index: number) => (
+      <li key={cn("skeleton", index)}>
+        <Skeleton className="startup-card_skeleton" />
+      </li>
+    ))}
+  </>
+);
 
 export default StartupCard
